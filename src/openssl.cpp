ee99132fc2534619422f4f8ea2222fd5710c8b21
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2021-10-20 00:38:59 +0800

    update http_server and openssl support update mime.conf

--- src/openssl.cpp
+++ src/openssl.cpp
@@ -149,7 +149,7 @@ namespace mongols
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
             this->ctx->freelist_max_len = 0;
 #endif
-            SSL_CTX_set_mode(this->ctx, SSL_MODE_RELEASE_BUFFERS | SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER | SSL_MODE_AUTO_RETRY);
+            SSL_CTX_set_mode(this->ctx, SSL_MODE_RELEASE_BUFFERS | SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER | SSL_MODE_AUTO_RETRY|SSL_MODE_ASYNC);
             SSL_CTX_set_options(this->ctx, flags);
             SSL_CTX_set_cipher_list(this->ctx, ciphers.c_str());
 
@@ -236,7 +236,7 @@ namespace mongols
         this->data = SSL_new(ctx);
         if (this->data)
         {
-            SSL_set_mode(this->data, SSL_MODE_RELEASE_BUFFERS | SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER);
+            SSL_set_mode(this->data, SSL_MODE_RELEASE_BUFFERS | SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER | SSL_MODE_AUTO_RETRY|SSL_MODE_ASYNC);
         }
     }
 
